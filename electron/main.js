727487a527fdd1fc1ca2740930ae2e5b2dbafa30
Video Toolbox Team <support@videotoolbox.com>
2025-01-13 02:33:53 +0800

    fix: application startup issues
    - Add preload script for secure IPC communication
    - Fix nodeIntegration and contextIsolation settings
    - Fix i18n translation file loading
    - Update to version 1.0.5

--- electron/main.js
+++ electron/main.js
@@ -92,8 +92,9 @@ const createWindow = () => {
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   });
 
